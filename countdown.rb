676f38a7e05bfc7ecc5a8b3e1b668d0f54ee2ff3
wolfene2001 <wolfene2001@yahoo.com>
2019-09-30 20:09:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,10 +4,10 @@ def countdown(number)
   number = 10
   while number >= 0
     puts "\n#{number} SECOND(S)!"
-
+    puts "HAPPY NEW YEAR!"
     number -= 1
 #    countdown_with_sleep
-    puts "HAPPY NEW YEAR!"
+    #puts "HAPPY NEW YEAR!"
 
   end
 end
